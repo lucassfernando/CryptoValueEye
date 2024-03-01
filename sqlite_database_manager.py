a1e3168a79b14e31e10a3c7cbf0e207a842cd64e
Lucas Fernando <lfssilva.18@gmail.com>
2024-03-01 20:37:07 -0300

    functions created update_values and consult_all_information

--- sqlite_database_manager.py
+++ sqlite_database_manager.py
@@ -10,8 +10,8 @@ class DataBase:
     def close(self):
         self.connection.close()
 
-    def create_table(self, table_name, table_columns):
-        self.cursor.execute(f'CREATE TABLE {table_name}({table_columns})')
+    def create_table(self, table_name, columns_name):
+        self.cursor.execute(f'CREATE TABLE {table_name}({columns_name})')
 
     def insert_data(self, selected_table, selecteds_columns, values):
         place_holders = ', '.join('?' * len(values))
@@ -19,8 +19,17 @@ class DataBase:
         self.cursor.execute(query, (values))
         self.connection.commit()
     
-    def check_column_exist_value(self, table_name, column, check_value):
-        self.cursor.execute(f"SELECT * FROM {table_name} WHERE {column} = '{check_value}'")
+    def check_column_exist_value(self, table_name, column_name, check_value):
+        self.cursor.execute(f"SELECT * FROM {table_name} WHERE {column_name} = '{check_value}'")
         result = self.cursor.fetchone()
         return result
 
+    def update_values(self, table_name, column_name, new_value, condition_column, condition_value):
+        query = f'UPDATE {table_name} SET {column_name} = ? WHERE {condition_column} = ?'
+        self.cursor.execute(query, (new_value, condition_value))
+        self.connection.commit()
+    
+    def consult_all_information(self, table_name):
+        self.cursor.execute(f'SELECT * FROM {table_name}')
+        result = self.cursor.fetchall()
+        return result
