06f70e07f20c7e64276fe1c850b94e3dbb68d22e
Lucas Fernando <lfssilva.18@gmail.com>
2024-03-10 09:06:30 -0300

    function monitor_climb created

--- start_app.py
+++ start_app.py
@@ -6,19 +6,26 @@ import threading
 
 from sqlite_database_manager import DataBase
 
+
+
+def monitor_climb(json, hours_ago, selected_percentage, return_text):
+
+    opened_cripto_value = json['data']['quotes'][hours_ago]['quote']['open'] 
+    closed_cripto_value = json['data']['quotes'][-1]['quote']['close']  
+    high_percentage = percentage_calculated(float(opened_cripto_value), float(closed_cripto_value))
+
+    if high_percentage >= selected_percentage:
+        return f'{return_text}: {high_percentage}'
+
+
 def percentage_calculated(initial_value, final_value):
 
     if final_value > initial_value:
 
         subtracting_values = final_value - initial_value
         percentage_value = (subtracting_values / initial_value) * 100
-        return 'high', percentage_value
+        return percentage_value
 
-    else:
-        
-        subtracting_values = initial_value - final_value
-        percentage_value = (subtracting_values / initial_value) * 100
-        return 'low', percentage_value
 
 def parse_float(val):
 
@@ -33,17 +40,12 @@ def start_check(cripto_id):
         if 'open' and 'close' in request.text:
 
             json_converted = json.loads(request.text, parse_float=parse_float)
-            #ABAIXO POSSO SELECIONAR QUALQUER PERIODO NO INTERVALO DE UMA HORA QUE EU QUISER, ENTÃO CONSIGO PEGAR O DE 6 HORAS SELECIONANDO -6 E COMPARANDO COM 1 HORA ATRAS PARA SABER A PORCENTAGEM
-            opened_cripto_value = json_converted['data']['quotes'][-1]['quote']['open'] 
-            closed_cripto_value = json_converted['data']['quotes'][-1]['quote']['close']  
-            high_or_low, percentage_value = percentage_calculated(float(opened_cripto_value), float(closed_cripto_value))
-
-            if high_or_low == 'high' and percentage_value >= 6.25:
-                print(f"CRIPTO: {cripto_id} = {opened_cripto_value} {closed_cripto_value} {round(percentage_value, 2)} {high_or_low}")
+            return_analyze = monitor_climb(json_converted, -1, 5.00, '1H')
+            print(return_analyze, cripto_id)
 
     except Exception as erro:
 
-        print(erro)
+        pass
 
 db = DataBase('CRIPTOS_INFO.db')
 all_cripto_data = db.consult_all_information('CRIPTOS_COINMARKETCAP')
